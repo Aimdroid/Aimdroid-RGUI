c48b627b8c39d8bed9331d1d64ec36499c7facda
Aimdroid <kenandevbox@gmail.com>
2025-08-29 16:42:58 -0600

    Update 24184676.lua
    Fixes and improvements

--- Extension/24184676.lua
+++ Extension/24184676.lua
@@ -23,12 +23,14 @@ page_exploits:AddToggle({
 	Callback = function(v)
 		global_hunterexploit = v
 		if global_hunterexploit then
+			game.Players.LocalPlayer.Character.HumanoidRootPart.Anchored=true
 			while global_hunterexploit and game.Players.LocalPlayer.Team==game.Teams.Zombies do
 				for _, v in game.Teams.Zombies:GetPlayers() do
 					RE.UserInputEvent:FireServer("LungeHit", v)
 				end
 				task.wait()
 			end
+			game.Players.LocalPlayer.Character.HumanoidRootPart.Anchored=false
 		end
 	end
 })
