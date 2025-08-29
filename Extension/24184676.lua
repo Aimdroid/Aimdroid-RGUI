e28cd34239c1705ce40912dfd2edd491e247f357
Aimdroid <kenandevbox@gmail.com>
2025-08-28 21:38:17 -0600

    Create 24184676.lua

---
+++ Extension/24184676.lua
@@ -0,0 +1,18 @@
+-- =========================================================
+-- EXPLOITS
+-- =========================================================
+page_exploits:AddToggle({
+	Position = UDim2.new(0,20,0,20),
+	LabelText = "Hunter Ability Exploit",
+	Callback = function(v)
+		global_hunterexploit = v
+		if global_hunterexploit then
+			while global_hunterexploit and game.Players.LocalPlayer.Team==game.Teams do
+				for _, v in game.Teams.Zombie:GetPlayers() do
+					game.ReplicatedStorage.RemoteEvents.UserInputEvent:FireServer("LungeHit", v)
+				end
+				task.wait()
+			end
+		end
+	end
+})
