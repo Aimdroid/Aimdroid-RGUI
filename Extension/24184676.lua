ed8e08009407a0d79b145913eb58ad054e44f068
Aimdroid <kenandevbox@gmail.com>
2025-08-28 22:38:14 -0600

    Update 24184676.lua

--- Extension/24184676.lua
+++ Extension/24184676.lua
@@ -1,20 +1,50 @@
 -- AKA LEFT 4 SURVIVAL
 
+-- =========================================================
+-- VARIABLES
+-- =========================================================
+
+RE = game.ReplicatedStorage.RemoteEvents
+
+-- =========================================================
+-- GLOBALS
+-- =========================================================
+
 global_hunterexploit=false
+global_soundspam=false
 
 -- =========================================================
 -- EXPLOITS
 -- =========================================================
+
 page_exploits:AddToggle({
-	Position = UDim2.new(0,20,0,20),
+	Position = UDim2.new(0,20,0,10),
 	LabelText = "Hunter Ability Exploit",
 	Callback = function(v)
 		global_hunterexploit = v
 		if global_hunterexploit then
 			while global_hunterexploit and game.Players.LocalPlayer.Team==game.Teams do
 				for _, v in game.Teams.Zombie:GetPlayers() do
-					game.ReplicatedStorage.RemoteEvents.UserInputEvent:FireServer("LungeHit", v)
+					RE.UserInputEvent:FireServer("LungeHit", v)
 				end
+				task.wait()
+			end
+		end
+	end
+})
+
+-- =========================================================
+-- MISC
+-- =========================================================
+
+page_misc:AddToggle({
+	Position = UDim2.new(0,20,0,10),
+	LabelText = "Sound Spam",
+	Callback = function(v)
+		global_soundspam = v
+		if global_soundspam then
+			while global_soundspam do
+				RE.UserInputEvent:FireServer("Lunge")
 				task.wait()
 			end
 		end
